2b98bb0fd0491b5607111cc11f10e5cdd33d4f8a
Victor Husak <lascari565758@gmail.com>
2026-03-03 23:25:53 +0200

    fix

--- sections/experts/experts-section.tsx
+++ sections/experts/experts-section.tsx
@@ -25,8 +25,6 @@ export const ExpertsSection: React.FC<ExpertsSectionProps> = (
         <PeopleCard className="w-[350px]" />
 
         <PeopleCard className="w-[350px]" />
-
-        <PeopleCard className="w-[350px]" />
       </div>
 
       {/* Footer */}
