d3beeb31c7376cb386cfe33fc734eb7ad22e79b0
Victor Husak <lascari565758@gmail.com>
2026-03-12 12:01:35 +0200

    conatiner

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayoutPage({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* Cookiebot */}
         <CookiebotScript />
