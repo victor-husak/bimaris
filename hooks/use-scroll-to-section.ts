d5648881881fd1041d74d4820df17c79b525473c
Victor Husak <lascari565758@gmail.com>
2026-03-18 14:29:44 +0200

    fix

--- hooks/use-scroll-to-section.ts
+++ hooks/use-scroll-to-section.ts
@@ -4,12 +4,12 @@ export type UseScrollToSectionProps = {
   offset: number;
 };
 
-export const useScrollToSection = (props: UseScrollToSectionProps) => {
+export const useScrollToSection = (props?: UseScrollToSectionProps) => {
   const onScrollToSection = useCallback((id: string) => {
     const el = document.getElementById(id);
     if (!el) return;
 
-    const yOffset = props.offset || -70; // высота header
+    const yOffset = props?.offset || -70; // высота header
     const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
 
     window.scrollTo({ top: y, behavior: "smooth" });
